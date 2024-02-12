f4a7e55f1c835b86347310c136bf9a63652b3f41
Sushant Sharma <sushant20.sharma00@gmail.com>
2024-02-12 19:46:29 +0530

    updated

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,6 +1,27 @@
-const RightSidebar = () => {
+import { fetchUsers } from "@/lib/controllers/user";
+import UserCard from "./UserCard";
+
+const RightSidebar = async () => {
+  const res = await fetchUsers();
+  const usersArray = res.slice(4);
+
   return (
-    <div className="bg-[#111111] sticky right-0 top-[80px] w-[350px] h-[calc(100vh-80px)] px-8 py-4 hidden 2xl:block"></div>
+    <div className="bg-[#111111] sticky right-0 top-[80px] w-[350px] h-[calc(100vh-80px)] px-8 py-4 hidden 2xl:block">
+      <h2 className="text-white text-3xl my-8">New Users:</h2>
+      <div className="flex flex-col gap-6">
+        {usersArray.map((user) => {
+          return (
+            <UserCard
+              key={user._id}
+              image={user.image}
+              name={user.name}
+              username={user.username}
+              userId={user.id}
+            />
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
